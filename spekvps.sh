024a273ce4e07fbcd6777d4068c0fe86a458e5a9
sean54321 <sean54321@users.noreply.github.com>
2017-03-10 19:33:32 +0700

    Update spekvps.sh

--- spekvps.sh
+++ spekvps.sh
@@ -3,7 +3,7 @@ echo "===============================================" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "Service"  | tee -a log-install.txt
 echo "-------"  | tee -a log-install.txt
-echo "OpenVPN  : TCP 1194 (client config : http://$MYIP/client.tar)"  | tee -a log-install.txt
+echo "OpenVPN  : TCP 1194 (client config : http://[IP]/client.tar)"  | tee -a log-install.txt
 echo "OpenSSH  : 22, 143"  | tee -a log-install.txt
 echo "Dropbear : 109, 110, 443, 80"  | tee -a log-install.txt
 echo "Squid3   : 8080 (limit to IP SSH)"  | tee -a log-install.txt
@@ -30,18 +30,16 @@ echo "7.) Mengecek Spesifikasi VPS Singkat Beserta Script : spekvps"  | tee -a l
 echo "8.) Mengecek Spesifikasi VPS Lengkap : benchnetwork"  | tee -a log-install.txt
 echo "9.) Mengecek Speed VPS : testspeed"  | tee -a log-install.txt
 echo "10.) Merestart VPS : reboot"  | tee -a log-install.txt
-echo ""
 echo ""  | tee -a log-install.txt
 echo "Fitur lain"  | tee -a log-install.txt
 echo "----------"  | tee -a log-install.txt
-echo "Webmin   : https://$MYIP:10000/"  | tee -a log-install.txt
-echo "vnstat   : http://$MYIP/vnstat/"  | tee -a log-install.txt
-echo "MRTG     : http://$MYIP/mrtg/"  | tee -a log-install.txt
-echo "OCS      : HTTP://$MYIP:2133/"  | tee -a log-install.txt
+echo "Webmin   : https://[IP]:10000/"  | tee -a log-install.txt
+echo "vnstat   : http://[IP]/vnstat/"  | tee -a log-install.txt
+echo "MRTG     : http://[IP]/mrtg/"  | tee -a log-install.txt
+echo "OCS      : HTTP://[IP]:2133/"  | tee -a log-install.txt
 echo "Timezone : Asia/Jakarta"  | tee -a log-install.txt
 echo "Fail2Ban : [on]"  | tee -a log-install.txt
 echo "IPv6     : [off]"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "Log Installasi --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo -e "Script by \e[1;33;44mSebastian Rahmad\e[0m"
