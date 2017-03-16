089a6f085a466ffe3a16b4e3ad037fdfd75841c9
sean54321 <sean54321@users.noreply.github.com>
2017-03-16 22:04:55 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -18,9 +18,10 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 service ssh restart
 
 # set repo
-wget -O /etc/apt/sources.list "https://raw.githubusercontent.com/sean54321/AmadRara/master/sources.list"
+wget -O /etc/apt/sources.list "https://raw.github.com/arieonline/autoscript/master/conf/sources.list.debian7"
 wget "http://www.dotdeb.org/dotdeb.gpg"
 cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+
 
 # remove unused
 apt-get -y --purge remove samba*;
@@ -61,11 +62,11 @@ echo "screenfetch" >> .profile
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/sean54321/AmadRara/master/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.github.com/arieonline/autoscript/master/conf/nginx.conf"
 mkdir -p /home/vps/public_html
 echo "<pre>Setup by Sebastian Rahmad <3 Mutiara Bunda</pre>" > /home/vps/public_html/index.html
 echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/sean54321/AmadRara/master/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.github.com/arieonline/autoscript/master/conf/vps.conf"
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 service php5-fpm restart
 service nginx restart
@@ -74,11 +75,11 @@ service nginx restart
 wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/arieonline/autoscript/master/conf/openvpn-debian.tar"
 cd /etc/openvpn/
 tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf "https://raw.githubusercontent.com/sean54321/AmadRara/master/1194.conf"
+wget -O /etc/openvpn/1194.conf "https://raw.github.com/arieonline/autoscript/master/conf/1194.conf"
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.up.rules "https://raw.githubusercontent.com/sean54321/AmadRara/master/iptables.up.rules"
+wget -O /etc/iptables.up.rules "https://raw.github.com/arieonline/autoscript/master/conf/iptables.up.rules"
 sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
 MYIP=`curl -s ifconfig.me`;
 MYIP2="s/xxxxxxxxx/$MYIP/g";
@@ -88,12 +89,12 @@ service openvpn restart
 
 # configure openvpn client config
 cd /etc/openvpn/
-wget -O /etc/openvpn/1194-client.ovpn "https://raw.githubusercontent.com/sean54321/AmadRara/master/1194-client.conf"
+wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/arieonline/autoscript/master/conf/1194-client.conf"
 sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
 PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
 useradd -M -s /bin/false KangArie
-echo "RahTia:$PASS" | chpasswd
-echo "RahTia" > pass.txt
+echo "KangArie:$PASS" | chpasswd
+echo "KangArie" > pass.txt
 echo "$PASS" >> pass.txt
 tar cf client.tar 1194-client.ovpn pass.txt
 cp client.tar /home/vps/public_html/
@@ -106,8 +107,8 @@ chmod +x /usr/bin/badvpn-udpgw
 screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
 
 # install mrtg
-wget -O /etc/snmp/snmpd.conf "https://raw.githubusercontent.com/sean54321/AmadRara/master/snmpd.conf"
-wget -O /root/mrtg-mem.sh "https://raw.githubusercontent.com/sean54321/AmadRara/master/mrtg-mem.sh"
+wget -O /etc/snmp/snmpd.conf "https://raw.github.com/arieonline/autoscript/master/conf/snmpd.conf"
+wget -O /root/mrtg-mem.sh "https://raw.github.com/arieonline/autoscript/master/conf/mrtg-mem.sh"
 chmod +x /root/mrtg-mem.sh
 cd /etc/snmp/
 sed -i 's/TRAPDRUN=no/TRAPDRUN=yes/g' /etc/default/snmpd
@@ -115,7 +116,7 @@ service snmpd restart
 snmpwalk -v 1 -c public localhost 1.3.6.1.4.1.2021.10.1.3.1
 mkdir -p /home/vps/public_html/mrtg
 cfgmaker --zero-speed 100000000 --global 'WorkDir: /home/vps/public_html/mrtg' --output /etc/mrtg.cfg public@localhost
-curl "https://raw.githubusercontent.com/sean54321/AmadRara/master/mrtg.conf" >> /etc/mrtg.cfg
+curl "https://raw.github.com/arieonline/autoscript/master/conf/mrtg.conf" >> /etc/mrtg.cfg
 sed -i 's/WorkDir: \/var\/www\/mrtg/# WorkDir: \/var\/www\/mrtg/g' /etc/mrtg.cfg
 sed -i 's/# Options\[_\]: growright, bits/Options\[_\]: growright/g' /etc/mrtg.cfg
 indexmaker --output=/home/vps/public_html/mrtg/index.html /etc/mrtg.cfg
@@ -158,7 +159,6 @@ apt-get -y install fail2ban;service fail2ban restart
 # install squid3
 apt-get update
 wget https://raw.githubusercontent.com/sean54321/AmadRara/master/squid.sh
-bash setup-squid.sh
 
 # install webmin
 cd
@@ -213,14 +213,9 @@ cp /root/bench-network.sh /usr/bin/benchnetwork
 chmod +x /usr/bin/benchnetwork
 
 # info penggunaan ram
-wget https://raw.githubusercontent.com/sean54321/AmadRara/master/info-used-ram
-cp /root/info-used-ram /usr/bin/usedram
+wget -O ps_mem.py "https://raw.github.com/pixelb/ps_mem/master/ps_mem.py"
+cp /root/ps_mem.py /usr/bin/usedram
 chmod +x /usr/bin/usedram
-
-# userlimit
-wget https://raw.githubusercontent.com/sean54321/AmadRara/master/userlimit.sh
-cp /root/userlimit.sh /usr/bin/userlimit
-chmod +x /usr/bin/userlimit
 
 # spek dan script singkat vps
 wget https://raw.githubusercontent.com/sean54321/AmadRara/master/spekvps.sh
@@ -275,8 +270,7 @@ echo "8.) Mengecek Spesifikasi VPS Lengkap : benchnetwork"  | tee -a log-install
 echo "9.) Mengecek Speed VPS : testspeed"  | tee -a log-install.txt
 echo "10.) Merestart VPS : reboot"  | tee -a log-install.txt
 echo "11.) Mengecek Penggunaan Ram VPS : usedram"  | tee -a log-install.txt
-echo "12.) Melimit User Yang Login Lebih Dari 1 Perangkat : userlimit1"  | tee -a log-install.txt
-echo "13.) Melimit User Yang Login Lebih Dari 2 Perangkat : userlimit2"  | tee -a log-install.txt
+echo "12.) Menambahkan Port Squid : bash squid.sh"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "Fitur lain"  | tee -a log-install.txt
 echo "----------"  | tee -a log-install.txt
