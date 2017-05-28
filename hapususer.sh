fa638ae0985946be98755b0e66604f709ba3e145
amadlovetia <amadloverara@gmail.com>
2017-05-28 18:53:13 +0700

    Create hapususer.sh

---
+++ hapususer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Script Delete Akun Modified by Sebastian Rahmad
+echo ""
+echo "  Selamat Datang Di Hapus User  "
+echo ""
+echo "--------------------------------"
+echo "USERNAME             EXP DATE   "
+echo "--------------------------------"
+while read mumetndase
+do
+        AKUN="$(echo $mumetndase | cut -d: -f1)"
+        ID="$(echo $mumetndase | grep -v nobody | cut -d: -f3)"
+        exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+        if [[ $ID -ge 1000 ]]; then
+        printf "%-17s %2s\n" "$AKUN" "$exp"
+        fi
+done < /etc/passwd
+echo "--------------------------------"
+echo ""
+read -p "Isikan Username : " Login
+userdel $Login
+echo ""
+echo "Akun $Login berhasil dihapus"
+JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+echo ""
+echo "--------------------------------"
+echo "Jumlah akun: $JUMLAH user"
+echo "--------------------------------"
